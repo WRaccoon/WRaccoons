f57676c1248939092a60990d4b3687212f71378b
agdubrov <alexey.dubrov@gmail.com>
2015-10-10 18:15:56 +0300

    request page and save to file

--- raccoonGraffiti/src/main/scala/com/ws/raccoon/Main.scala
+++ raccoonGraffiti/src/main/scala/com/ws/raccoon/Main.scala
@@ -1,12 +1,17 @@
 package com.ws.raccoon
 
+import java.io.{FileOutputStream, InputStream}
+
+import com.ws.raccoon.services.RequestService
+import org.apache.commons.io.IOUtils
+
 /**
  * Created by agdubrov on 10/10/15.
  */
 object Main {
 
   def main(args: Array[String]) {
-    println("Hello, world!");
+    RequestService.storeHtml("http://solidskills.se");
   }
 
 }
